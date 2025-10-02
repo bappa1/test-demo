90f17cd768c9ea56db9d8ef6e5e6768d2cb94ff2
Bappa Pal <bappapal121@gmail.com>
2025-10-02 15:12:02 +0530

    Changes in JS file

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
 function test(){
+    let a;
+    let b = 10;
+    let c = 50;
+    c = b*c;
+    console.log("The multiplication of two numbers:::===="+c);
     console.log("This is test function");
 }
